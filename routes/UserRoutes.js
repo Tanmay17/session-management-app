b45e59fb015d25322e5e3b2d39a168f198564044
Tanmay <tanmay.srivastava@cred.club>
2024-07-04 21:22:35 +0530

    chore: initial setup

---
+++ routes/UserRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/createUser', null);
+router.get('/doGetUser/:mobile', null);
+router.post('/loginUser', null);
+
+module.exports = router;
